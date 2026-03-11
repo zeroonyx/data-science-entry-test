f3549c809f3a9db30d7534ac02bce0e407f0cbbb
zeroonyx <zhongyili@gmail.com>
2026-03-11 15:36:25 +0000

    q2 answer

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,20 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+    if not isinstance(lst, list):
+        raise TypeError("please input a list")
+    # iterate and replace, using enumerate to get index and value together.
+    for i, v in enumerate(lst):
+        if v == find_val:
+            lst[i] = replace_val
+    return lst
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+
+# test scenarios
+print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
+print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
